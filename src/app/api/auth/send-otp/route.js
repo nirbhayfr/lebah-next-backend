48bcbaa0bda9effc43f15583d3d96094a46e290e
Nirbhay Chauhan <nirbhaychauhan0306@gmail.com>
2026-02-22 11:23:32 +0530

    //

--- src/app/api/auth/send-otp/route.js
+++ src/app/api/auth/send-otp/route.js
@@ -1,41 +1,49 @@
 import { connectDB } from "@/lib/db";
 import { createResponse, ErrorResponse } from "@/lib/responseWrapper";
+import { sendOtpEmail } from "@/lib/sendOtpEmail";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(req) {
-    try {
-     await connectDB();
-    let { email } = await req.json();
+	try {
+		await connectDB();
+		let { email } = await req.json();
 
-    if (!email) {
-      return Response.json(ErrorResponse(400, "Email is required"), { status: 400 });
-    }
+		if (!email) {
+			return Response.json(ErrorResponse(400, "Email is required"), {
+				status: 400,
+			});
+		}
 
-    email = email.toLowerCase().trim();
-    let user = await User.findOne({ email });
-    if (!user) {
-      user = new User({ email });
-    }
+		email = email.toLowerCase().trim();
+		let user = await User.findOne({ email });
+		if (!user) {
+			user = new User({ email });
+		}
 
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    if (user?.otpExpiry && user.otpExpiry > Date.now()) {
-      return Response.json({
-        status: "error",
-        message: "Please wait before resending OTP",
-      },
-      { status: 429 }
-    );
-    }
+		const otp = Math.floor(100000 + Math.random() * 900000).toString();
+		if (user?.otpExpiry && user.otpExpiry > Date.now()) {
+			return Response.json(
+				{
+					status: "error",
+					message: "Please wait before resending OTP",
+				},
+				{ status: 429 },
+			);
+		}
 
-    user.otp = await bcrypt.hash(otp, 10);
-    user.otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
-    await user.save();
-    await sendOtpEmail(email, otp);
+		user.otp = await bcrypt.hash(otp, 10);
+		user.otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
+		await user.save();
+		await sendOtpEmail(email, otp);
 
-    return Response.json(createResponse(200, null, "OTP sent successfully"));
-  } catch (error) {
-    console.error("SEND OTP ERROR:", error);
-    return Response.json(ErrorResponse(500, "Internal server error"), { status: 500 });
-  }
+		return Response.json(
+			createResponse(200, null, "OTP sent successfully"),
+		);
+	} catch (error) {
+		console.error("SEND OTP ERROR:", error);
+		return Response.json(ErrorResponse(500, "Internal server error"), {
+			status: 500,
+		});
+	}
 }
