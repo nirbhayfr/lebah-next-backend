00c1548ad7751fe0d7e5b175d23067293ed385ee
Nirbhay Chauhan <nirbhaychauhan0306@gmail.com>
2026-02-22 11:31:47 +0530

    //

--- src/models/User.js
+++ src/models/User.js
@@ -1,45 +1,44 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-    },
+	{
+		name: {
+			type: String,
+		},
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
 
-    password: {
-      type: String,
-      required: true,
-    },
+		password: {
+			type: String,
+			// required: true,
+		},
 
-    role: {
-      type: String,
-      enum: ["ADMIN", "CUSTOMER"],
-      default: "CUSTOMER",
-    },
+		role: {
+			type: String,
+			enum: ["ADMIN", "CUSTOMER"],
+			default: "CUSTOMER",
+		},
 
-    otp: {
-      type: String,
-    },
+		otp: {
+			type: String,
+		},
 
-    otpExpiry: {
-      type: Date,
-    },
+		otpExpiry: {
+			type: Date,
+		},
 
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true },
 );
-
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
